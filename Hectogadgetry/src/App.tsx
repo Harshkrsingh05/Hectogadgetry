a187269c791b65c327ef91a0ade9862d04e01590
Harsh kumar <harshkumarsingh005@gmail.com>
2023-11-16 00:43:58 +0530

    added components

---
+++ Hectogadgetry/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello jii</h1>
+      <h5>AB Krenge Development.</h5>
+    </>
+  )
+}
+
+export default App
