40af5e29fc462b2439c204c85e946c45b9f0a8ca
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-11-21 11:04:25 +0530

    Add files via upload

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+// mongoose.connect('your-mongodb-connection-string');
+
+// Routes and other server logic go here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
