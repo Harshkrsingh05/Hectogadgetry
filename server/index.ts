29289cb1108bd5c1103800d65bb8d5cbaad5088d
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-11-22 19:43:55 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+//Backend is pending.
 // MongoDB connection
 // mongoose.connect('your-mongodb-connection-string');
 
